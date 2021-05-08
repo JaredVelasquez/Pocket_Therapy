f0f624b41f2b59a575403e4d2e9f24d36a6cb3f2
Norman-Villalvir5 <normanvillalvir5@gmail.com>
2021-05-07 22:12:23 -0600

    Elaboracion de vistas, utilizando INNER JOIN

---
+++ DBO/Database/Query_View/View.sql
@@ -0,0 +1,16 @@
+SELECT us.lastName  'Nombre del Usuario ', ha.name ' Nombre del Hashtag', po.contentPost 'Contenido del Post ',co.comentcnt' Coment',co.likeacnt'Like '
+
+FROM [User] us
+
+INNER JOIN Hashtag ha
+
+ON us.user_id = ha.hastag_id
+
+INNER JOIN Post po
+
+ON po.post_id = po.post_id
+
+
+INNER JOIN Comunity co
+
+on co.user_id = co.user_id
