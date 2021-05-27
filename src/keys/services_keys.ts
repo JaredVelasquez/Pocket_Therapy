01ce8b6b34ef8d6fabfa2bf67d82201aab5121f0
JaredVelasquez <jared.velasquez1122@gmail.com>
2021-05-27 07:11:01 -0600

    corrigiendo variables de entorno

--- src/keys/services_keys.ts
+++ src/keys/services_keys.ts
@@ -7,11 +7,7 @@ export namespace Servicekeys {
   export const TOKEN_EXPIRATION_TIME = (Date.now() / MILLISECONDS) + (ONE_HOUR_SECONDS * 3);
   export const SENDER_EMAIL = 'JaredVelasquez@unitec.edu';
   export const SENDER_PHONE_NUMBER = '+50494978931';
-  export const SENDGRID_TOKEN = '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';
   export const GENERATE_NEW_VERIFY_CODE = require('codeid');
   export const TIME_OUT_NAME = 'timeout_event';
   export const VERIFICATION_CODE_NAME = 'code';
-  export const CLOUDINARY_NAME = 'jaredvelasquez';
-  export const CLOUDINARY_API_KEY = '498742159759711';
-  export const CLOUDINARY_API_SECRET = 'dRH1kYrR6mO2NqtsPXu_rgk58No';
 }
