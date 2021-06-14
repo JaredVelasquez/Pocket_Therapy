74e19874766f398dddcf12aa589e3e595c632175
JaredVelasquez <jared.velasquez1122@gmail.com>
2021-06-14 09:24:03 -0600

    solucionando error al obtener comentarios

--- src/keys/viewOf.keys.ts
+++ src/keys/viewOf.keys.ts
@@ -3,5 +3,5 @@ export namespace ViewOf {
   export const GetUserLikes = `SELECT * FROM [Like] WHERE like_id = `;
   export const GetContact = `SELECT * FROM dbo.GetContact`;
   export const GetShare = `SELECT * FROM dbo.GetShare`;
-  export const GetComment = `SELECT * FROM dbo.GetComment`;
+  export const GetComment = `SELECT * FROM dbo.GetComment WHERE PostId = `;
 }
